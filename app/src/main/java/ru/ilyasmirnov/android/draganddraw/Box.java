273685d967232f84a37698b23008ed3f617c7c03
Smirnov Ilya <smirnovilya19861010@gmail.com>
2019-06-04 18:59:28 +0300

    Before exercise on page 619 (State saving) _ 1

---
+++ app/src/main/java/ru/ilyasmirnov/android/draganddraw/Box.java
@@ -0,0 +1,4 @@
+package ru.ilyasmirnov.android.draganddraw;
+
+public class Box {
+}
---
+++ app/src/main/java/ru/ilyasmirnov/android/draganddraw/BoxDrawingView.java
@@ -0,0 +1,6 @@
+package ru.ilyasmirnov.android.draganddraw;
+
+import android.view.View;
+
+public class BoxDrawingView extends View {
+}
